f54d1213547da9b19312342e4f493ee78aec1e9c
Dmitry DGSC <dgsc.develop@gmail.com>
2019-10-05 13:27:17 +0300

    init git

---
+++ app/src/main/java/com/moidoc/example/android/flickrverysimpleclientexample/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.moidoc.example.android.flickrverysimpleclientexample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.moidoc.example.android.flickrverysimpleclientexample.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/moidoc/example/android/flickrverysimpleclientexample/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.moidoc.example.android.flickrverysimpleclientexample.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
